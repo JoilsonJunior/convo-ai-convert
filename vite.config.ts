ebec44adaebaaea8db373fd178fc70de048cc1af
JoilsonJunior <joilson320@hotmail.com>
2025-06-25 00:53:11 -0300

    Atualização para subir o site online

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/convo-ai-convert/", // 👈 ESSA LINHA É ESSENCIAL
   server: {
     host: "::",
     port: 8080,
